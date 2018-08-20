b6f91666a74f9e29349a2db9769e394f5d440bd5
shoham-elad <40741297+shoham-elad@users.noreply.github.com>
2018-08-20 23:56:32 +0300

    Update project.sh

--- finals/old/project.sh
+++ finals/old/project.sh
@@ -2,11 +2,11 @@
 #train/test data num_of_rows
 if [$1 = 'train']
 then
-	python flow.py flow.py $2 $3
+	python flow.py $2 $3
 else
 	if [$1 = 'test']
 	then
-		python testFlow.py flow.py $2 $3
+		python testFlow.py $2 $3
 	else 
 		echo "not a command. please write train or test....."
 	fi
